fcd57c20f9d52ce2a309572c807e39d2161ce0ce
maluk <malukhin2003@list.ru>
2022-10-21 09:25:38 +0300

    kurs

--- shop_game/shop_game/settings.py
+++ shop_game/shop_game/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'page',
+    'users',
+    'crispy_forms',
 ]
 
 MIDDLEWARE = [
@@ -124,3 +126,11 @@ STATICFILES_DIRS = ['static']
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+LOGIN_REDIRECT_URL = 'home'
+
+LOGOUT_REDIRECT_URL = 'home'
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+TIME_ZONE = 'Asia/Yekaterinburg'
--- shop_game/shop_game/urls.py
+++ shop_game/shop_game/urls.py
@@ -20,4 +20,7 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('', include('page.urls')),
     path('admin/', admin.site.urls),
+    path('users/', include('users.urls')),
+    path('users/', include('django.contrib.auth.urls')),
+
 ]
